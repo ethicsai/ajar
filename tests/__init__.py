0a58c2d7cc5fa7a4f09414f4be89ed61533467ed
rchaput <rchaput.pro@gmail.com>
2023-09-20 16:53:03 +0200

    Add tests

---
+++ tests/__init__.py
@@ -0,0 +1,9 @@
+"""
+Unit test package for AJAR
+
+Run all tests in this package with `python -m unittest` (executed at the
+project root directory).
+
+All filenames must match the `test_*.py` pattern, otherwise they will not be
+found and executed!
+"""
